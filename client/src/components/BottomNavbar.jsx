9a631d6f6fe1c35d61cafc37adc3f0482906895e
hughes-curtis <119542134+hughes-curtis@users.noreply.github.com>
2025-02-16 16:18:38 +0300

    add create button to bottom nav

--- client/src/components/BottomNavbar.jsx
+++ client/src/components/BottomNavbar.jsx
@@ -1,12 +1,18 @@
 import { Link } from "react-router-dom";
-import { FaDollarSign, FaUserLarge } from "react-icons/fa6";
+import { FaDollarSign, FaPlus, FaUserLarge } from "react-icons/fa6";
 import { FaThList } from "react-icons/fa";
+import { PlusCircle, PlusSquare } from "lucide-react";
 
 const BottomNavbar = ({ user }) => {
   if (!user) return null; // Hide if user isn't logged in
 
   return (
     <div className="fixed bottom-0 left-0 w-full bg-white shadow-md border-t flex justify-around items-center py-3 md:hidden">
+      <Link to="/create" className="flex flex-col items-center text-gray-700 hover:text-fuchsia-700">
+        <PlusSquare className="text-xl" />
+        <span className="text-xs">Create</span>
+      </Link>
+
       <Link to="/posts" className="flex flex-col items-center text-gray-700 hover:text-fuchsia-700">
         <FaThList className="text-xl" />
         <span className="text-xs">Posts</span>
