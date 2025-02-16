565f89f5d13b9a294176ab377bc6f9b1a1198789
hughes-curtis <119542134+hughes-curtis@users.noreply.github.com>
2025-02-16 13:21:11 +0300

    landing page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Landing() {
   return (
-    <div className='h-screen flex flex-col justify-between mx-1'>
+    <div className='h-[100dvh] flex flex-col justify-between mx-1'>
         <div className='bg-slate-200'>Navbar</div>
         <div className='w-full text-center text-4xl py-2'>
             PESKAYA
@@ -14,9 +14,9 @@ function Landing() {
                 <h1>Sell anything</h1>
             </div>
             <div className=''>
-                <h1 className='text-center text-3xl mb-5'>Join Today!</h1>
-                <button className='border-2 text-xl px-3 py-3 w-full mb-2 font-semibold rounded-md'>Log In</button>
-                <button className='bg-black text-white text-xl font-semibold px-3 py-3 rounded-md w-full'>Create Account</button>
+                <h1 className='text-center text-xl mb-5'>Join Today!</h1>
+                <button className='border-2 text-lg px-3 py-3 w-full mb-2 font-semibold rounded-md'>Log In</button>
+                <button className='bg-black text-white text-lg font-semibold px-3 py-3 rounded-md w-full'>Create Account</button>
             </div>
 
         </div>
