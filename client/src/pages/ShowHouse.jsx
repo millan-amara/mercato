d9f6dba9df58f071b4987e30cfeacec4f7ebb46f
Millan Amara <119542134+millan-amara@users.noreply.github.com>
2025-04-25 23:41:49 +0300

    adjust house show page ui

--- client/src/pages/ShowHouse.jsx
+++ client/src/pages/ShowHouse.jsx
@@ -81,7 +81,7 @@ function ShowHouse() {
   return (
     <>
     <Navbar />
-    <main className='mb-24 px-2'>
+    <main className='mb-24 px-2 mt-8'>
       <Swiper 
             modules={[Navigation, Pagination, Scrollbar, A11y]}
             pagination={{clickable: true}}
@@ -92,15 +92,15 @@ function ShowHouse() {
                     <div
                       className="relative flex justify-center" 
                     >
-                      <img src={url.url} className="max-h-80" alt={house.title} />
+                      <img src={url.url} className="max-h-80 rounded-lg" alt={house.title} />
                     </div>
                 </SwiperSlide>
             ))}
         </Swiper>
         {user && user._id === house.author &&
-        <div className='flex justify-center'>
-          <Link to={`/houses/${house._id}/edit`} className='bg-green-300 mr-2 rounded-md w-1/3 md:w-1/5 py-2 text-center'>Edit</Link>
-          <button onClick={handleDelete} className='bg-orange-400 ml-2 rounded-md w-1/3 md:w-1/5 py-2 text-white'>Delete</button>
+        <div className='flex justify-center mt-4'>
+          <Link to={`/houses/${house._id}/edit`} className='bg-green-300 hover:bg-green-400 mr-2 rounded-md w-1/3 md:w-1/6 py-2 text-center'>Edit</Link>
+          <button onClick={handleDelete} className='bg-orange-400 hover:bg-orange-500 ml-2 rounded-md w-1/3 md:w-1/6 py-2 text-white'>Delete</button>
         </div>}
 
         <div
@@ -162,7 +162,7 @@ function ShowHouse() {
             </>
           )}
         </div> */}
-        <div id="specs" className='mt-8'>
+        <div id="specs" className='mt-8 lg:w-1/2 mx-auto'>
           <h1 className='text-xl lg:text-3xl font-medium mb-8 text-center'>More Details</h1>
           <div
             className='' 
